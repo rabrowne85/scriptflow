bdfa460ae4e192c10498c02c1e8d23f88bc1dd56
Richard Browne <r.a.browne@gmail.com>
2024-07-19 15:50:33 +0100

    💅 Added an extra gradient stop

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,7 +21,7 @@
 
 <body class="h-full font-sans antialiased xl:overscroll-none w-full text-gray-100">
     <div class="mx-auto max-w-7xl px-0 sm:px-6 lg:px-8 min-h-full relative pb-28">
-        <h1 class="absolute top-0 left-12 text-6xl h-full lg:left-20 xl:left-32 overflow-visible items-center justify-center select-none hidden md:flex lg:text-7xl xl:text-8xl font-semibold bg-gradient-to-b from-rose-400 leading-none to-rose-600 bg-clip-text text-transparent stroke-1 stroke-rose-50 [text-orientation:mixed] [writing-mode:vertical-rl] rotate-180 font-['MarckScript'] tracking-wider">ScriptFlow.cc</h1>
+        <h1 class="absolute top-0 left-12 text-6xl h-full lg:left-20 xl:left-32 overflow-visible items-center justify-center select-none hidden md:flex lg:text-7xl xl:text-8xl font-semibold bg-gradient-to-b from-cyan-400 via-rose-400 leading-none to-rose-600 bg-clip-text text-transparent stroke-1 stroke-rose-50 [text-orientation:mixed] [writing-mode:vertical-rl] rotate-180 font-['MarckScript'] tracking-wider">ScriptFlow.cc</h1>
         @inertia
     </div>
 </body>
