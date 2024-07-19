76f5e606b0bf8748222764d7a7fd965c455db3c0
Richard Browne <r.a.browne@gmail.com>
2024-07-19 15:00:20 +0100

    💅 Update footer to point to correct links

--- resources/js/Layouts/Base.vue
+++ resources/js/Layouts/Base.vue
@@ -4,25 +4,22 @@ import {defineComponent, h} from 'vue';
 const navigation = {
     social: [
         {
-            name: 'Website',
-            href: 'https://www.vulpecula.io',
+            name: 'Sponsor me',
+            href: 'https://github.com/sponsors/rabrowne85',
             icon: defineComponent({
                 render: () => h('svg', {
-                    stroke: 'currentColor',
+                    fill: 'currentColor',
                     viewBox: '0 0 24 24',
-                    'stroke-width': 1.5,
                 }, [
                     h('path', {
-                        'stroke-linecap': 'round',
-                        'stroke-linejoin': 'round',
-                        d: 'm20.893 13.393-1.135-1.135a2.252 2.252 0 0 1-.421-.585l-1.08-2.16a.414.414 0 0 0-.663-.107.827.827 0 0 1-.812.21l-1.273-.363a.89.89 0 0 0-.738 1.595l.587.39c.59.395.674 1.23.172 1.732l-.2.2c-.212.212-.33.498-.33.796v.41c0 .409-.11.809-.32 1.158l-1.315 2.191a2.11 2.11 0 0 1-1.81 1.025 1.055 1.055 0 0 1-1.055-1.055v-1.172c0-.92-.56-1.747-1.414-2.089l-.655-.261a2.25 2.25 0 0 1-1.383-2.46l.007-.042a2.25 2.25 0 0 1 .29-.787l.09-.15a2.25 2.25 0 0 1 2.37-1.048l1.178.236a1.125 1.125 0 0 0 1.302-.795l.208-.73a1.125 1.125 0 0 0-.578-1.315l-.665-.332-.091.091a2.25 2.25 0 0 1-1.591.659h-.18c-.249 0-.487.1-.662.274a.931.931 0 0 1-1.458-1.137l1.411-2.353a2.25 2.25 0 0 0 .286-.76m11.928 9.869A9 9 0 0 0 8.965 3.525m11.928 9.868A9 9 0 1 1 8.965 3.525',
+                        d: 'm11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z',
                     }),
                 ]),
             }),
         },
         {
             name: 'GitHub',
-            href: 'https://github.com/vulpecula-io',
+            href: 'https://github.com/rabrowne85/scriptflow',
             icon: defineComponent({
                 render: () => h('svg', {
                     fill: 'currentColor',
@@ -41,20 +38,20 @@ const navigation = {
 </script>
 
 <template>
-        <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
-        <div class="mx-auto max-w-lg">
-            <slot/>
-        </div>
-        <footer class="absolute bottom-8 w-full left-0 ">
-            <div class="border-t border-white/10 px-6 pt-8 md:flex md:items-center md:justify-between">
-                <div class="flex space-x-6 md:order-2">
-                    <a v-for="item in navigation.social" :key="item.name" :href="item.href"
-                       class="text-slate-500 hover:text-rose-400">
+    <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
+    <div class="mx-auto max-w-lg">
+        <slot/>
+    </div>
+    <footer class="absolute bottom-8 w-full left-0 ">
+        <div class="border-t border-white/10 px-6 pt-8 md:flex md:items-center md:justify-between">
+            <div class="flex space-x-6 md:order-2">
+                <a v-for="item in navigation.social" :key="item.name" :href="item.href"
+                   class="text-slate-500 hover:text-rose-400">
                         <span class="sr-only">{{ item.name }}</span>
                         <component :is="item.icon" class="size-6" aria-hidden="true"/>
                     </a>
                 </div>
-                <p class="mt-8 text-xs leading-5 text-slate-400 md:order-1 md:mt-0">&copy; {{ new Date().getFullYear() }} Vulpecula Limited. All rights reserved.</p>
+                <p class="mt-8 text-xs leading-5 text-slate-400 md:order-1 md:mt-0">&copy; {{ new Date().getFullYear() }} Richard Browne. All rights reserved.</p>
             </div>
         </footer>
 </template>
